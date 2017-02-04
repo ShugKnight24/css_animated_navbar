57e74c2e3a10ba924a72ad1aee2bacadb4a2a407
Shugmi Shumunov <SShumunov@gmail.com>
2017-02-04 18:15:27 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# CSS Navigation Animation
+
+In this project, I created 5 different types of animations for a navbar.
+
+Best viewed on a screen width greater than 866px so that the animations do not overlap.
+
+### Still To-Do:
+- [ ] Make fully responsive
+- [ ] Add additional animations
---
+++ js/script.js
@@ -0,0 +1,114 @@
+$(document).ready(function(){
+
+    
+    // list item 1 mouse enter
+    $(".drop-menu>li:nth-child(1)").mouseenter(function(){
+        var $this = $(this);
+        $this.text("один");
+    });
+    // list item 1 mouse leave
+    $(".drop-menu>li:nth-child(1)").mouseleave(function(){
+        var $this = $(this);
+        $this.text("ahdeen");
+    });
+
+    // list item 2 mouse enter
+    $(".drop-menu>li:nth-child(2)").mouseenter(function(){
+        var $this = $(this);
+        $this.text("два");
+    });
+    // list item 2 mouse leave
+    $(".drop-menu>li:nth-child(2)").mouseleave(function(){
+        var $this = $(this);
+        $this.text("dvah");
+    });
+
+    // list item 3 mouse enter
+    $(".drop-menu>li:nth-child(3)").mouseenter(function(){
+        var $this = $(this);
+        $this.text("три");
+    });
+    // list item 3 mouse leave
+    $(".drop-menu>li:nth-child(3)").mouseleave(function(){
+        var $this = $(this);
+        $this.text("tree");
+    });
+
+    // list item 4 mouse enter
+    $(".drop-menu>li:nth-child(4)").mouseenter(function(){
+        var $this = $(this);
+        $this.text("четыре");
+    });
+    // list item 4 mouse leave
+    $(".drop-menu>li:nth-child(4)").mouseleave(function(){
+        var $this = $(this);
+        $this.text("chetiryeh");
+    });
+
+    // list item 5 mouse enter
+    $(".drop-menu>li:nth-child(5)").mouseenter(function(){
+        var $this = $(this);
+        $this.text("пять");
+    });
+    // list item 5 mouse leave
+    $(".drop-menu>li:nth-child(5)").mouseleave(function(){
+        var $this = $(this);
+        $this.text("pyat'");
+    });
+
+    // list item 6 mouse enter
+    $(".drop-menu>li:nth-child(6)").mouseenter(function(){
+        var $this = $(this);
+        $this.text("шесть");
+    });
+    // list item 6 mouse leave
+    $(".drop-menu>li:nth-child(6)").mouseleave(function(){
+        var $this = $(this);
+        $this.text("shest'");
+    });
+
+    // list item 7 mouse enter
+    $(".drop-menu>li:nth-child(7)").mouseenter(function(){
+        var $this = $(this);
+        $this.text("семь");
+    });
+    // list item 7 mouse leave
+    $(".drop-menu>li:nth-child(7)").mouseleave(function(){
+        var $this = $(this);
+        $this.text("syem'");
+    });
+
+    // list item 8 mouse enter
+    $(".drop-menu>li:nth-child(8)").mouseenter(function(){
+        var $this = $(this);
+        $this.text("восемь");
+    });
+    // list item 8 mouse leave
+    $(".drop-menu>li:nth-child(8)").mouseleave(function(){
+        var $this = $(this);
+        $this.text("vohsyem'");
+    });
+
+    // list item 9 mouse enter
+    $(".drop-menu>li:nth-child(9)").mouseenter(function(){
+        var $this = $(this);
+        $this.text("девять");
+    });
+    // list item 9 mouse leave
+    $(".drop-menu>li:nth-child(9)").mouseleave(function(){
+        var $this = $(this);
+        $this.text("dyehvit'");
+    });
+
+    // list item 10 mouse entere
+    $(".drop-menu>li:nth-child(10)").mouseenter(function(){
+        var $this = $(this);
+        $this.text("десять");
+    });
+    // list item 10 mouse leavee
+    $(".drop-menu>li:nth-child(10)").mouseleave(function(){
+        var $this = $(this);
+        $this.text("dyehsit'");
+    });
+
+});
